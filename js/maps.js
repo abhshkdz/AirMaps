d766a4e313b83c1334f6bf557f10851620de6434
Abhishek <das.abhshk@gmail.com>
2014-02-09 03:32:58 +1000

    Adds method for nearby places

--- js/maps.js
+++ js/maps.js
@@ -1,11 +1,21 @@
-var getLatLng = function (geo, address) {
-	var address = "New Delhi"
+var getNearbyPlaces = function (latitude, longitude, cb) {
+	var key = "AIzaSyAnBfpbJGNiAlh5TFu-V5UHglUEgK8DbzY";
 	$.ajax({
-	  url:"http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false",
+	  url:"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ latitude + "," + longitude + "&radius=5000&types=amusement_park|aquarium|art_gallery|city_hall|library|museum|place_of_worship|stadium|university|zoo&sensor=false&key="+key,
 	  type: "POST",
 	  success: function(res){
-	     console.log(res.results[0].geometry.location.lat);
-	     console.log(res.results[0].geometry.location.lng);
+	  	var jsonObject = JSON.parse(res);
+	  	var results = jsonObject.results;
+	  	var nearby = array();
+	  	for(i=0; i< results.length; i++){
+	  		var r = {};
+	  		r.latitude  = nearby[i].geometry.location.lat;
+	  		r.longitude = nearby[i].geometry.location.lng;
+	  		r.name = nearby[i].name;
+	  		r.reference = nearby[i].reference;
+	  		nearby.push(r);
+	  	}
+	  cb(nearby);
 	  }
 	});
 }

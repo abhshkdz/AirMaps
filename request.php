f99d6d41a1d6e3de6915d1c24869b47b8a804043
Divij Bindlish <dvjbndlsh93@gmail.com>
2014-02-09 04:36:30 +0530

    changes radius

--- request.php
+++ request.php
@@ -3,7 +3,7 @@
 	$lat = $_GET['lat'];
 	$lng = $_GET['lng'];
 	$key = $_GET['key'];
-	$url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=5000&types=amusement_park|aquarium|art_gallery|city_hall|library|museum|place_of_worship|stadium|university|zoo&sensor=false&key=$key";
+	$url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=10000&types=amusement_park|aquarium|art_gallery|city_hall|library|museum|place_of_worship|stadium|university|zoo&sensor=false&key=$key";
 	$res = file_get_contents($url);
 	echo json_encode($res);
 
